11ef5c3f22b2493f0f74a276d20506962214fc3e
baniyanoor-svg <baniyanoor@gmail.com>
2025-09-22 21:03:58 +0530

    Add files via upload

---
+++ CPP/S3P5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+    float radius;
+public:
+    Circle() {   
+
+
+        radius = 1;
+    }
+    Circle(float r) { 
+        radius = r;
+    }
+    void area() {
+        cout << "Area = " << 3.14 * radius * radius << endl;
+    }
+};
+
+int main() {
+    Circle c1;       
+    Circle c2(5);     
+
+    c1.area();
+    c2.area();
+    return 0;
+}

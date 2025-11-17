62d61eb6665b3eb4c68d34372acc5ead9ba88e27
baniyanoor-svg <baniyanoor@gmail.com>
2025-11-17 22:57:53 +0530

    Add files via upload

---
+++ CPP/S6P6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void speak() {       
+        cout << "Animal speaks" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void speak() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void speak() {
+        cout << "Cat meows" << endl;
+    }
+};
+
+int main() {
+    Animal *ptr;
+
+    Dog d;
+    Cat c;
+
+    ptr = &d;
+    ptr->speak();     
+
+    ptr = &c;
+    ptr->speak();     
+
+    return 0;
+}

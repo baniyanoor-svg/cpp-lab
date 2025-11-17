97be11a9d1d81f030d212bba1f20c43352fb9819
baniyanoor-svg <baniyanoor@gmail.com>
2025-11-17 23:00:00 +0530

    Add files via upload

---
+++ CPP/S7P1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+template <class T>
+void printTwo(T a,T b){
+
+    cout<<a<<" "<<b;
+    
+}
+int main(){
+int x,y;
+cout<<"enter x :";
+cin>>x;
+cout<<"enter y :";
+cin>>y;
+printTwo(x,y);
+
+
+
+
+}
